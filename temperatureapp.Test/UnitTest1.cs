40250da0860ee26cf446e65ef8b4ba7c0d973439
domincbas <daryllbastienne32@gmail.com>
2024-11-22 01:59:36 +1300

    Learnt about arrange act assert pattern. used theory and inline data attributes

--- temperatureapp.Test/UnitTest1.cs
+++ temperatureapp.Test/UnitTest1.cs
@@ -34,5 +34,47 @@ public class UnitTest1
         Assert.NotEqual(5, result);
     }
 
+    [Fact]
+    public void CanAddNumbers()
+    {
+        //Arrange
+        int num1 = 2;
+        int num2 = 3;
+
+        //Act
+        int result = num1 + num2;
+
+        //Assert
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void CanCaulculateAverage()
+    {
+        //Arrange
+        int[] numbers = {2, 3, 5, 7, 11};
+
+        //Act
+        double average = numbers.Average();
+
+        //Assert
+        Assert.Equal(5.6, average, 1);
+    }
+
+    [Theory]
+    [InlineData(3, 7, 10)]
+    [InlineData(-3, -7, -10)]
+    [InlineData(100, 200, 300)]
+    public void CanAddNumberParameterized(int num1, int num2, int expected)
+    {
+        //Assert
+
+        //Act
+        int result = num1 + num2;
+
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
 
 }
