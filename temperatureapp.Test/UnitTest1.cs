dd067fd3735d80c4120ba8ff6c08ffbcb9525c04
domincbas <daryllbastienne32@gmail.com>
2024-11-22 01:42:39 +1300

    Assert methods using equal and not equal operator

--- temperatureapp.Test/UnitTest1.cs
+++ temperatureapp.Test/UnitTest1.cs
@@ -20,5 +20,19 @@ public class UnitTest1
         Assert.True(true);
     }
 
+    [Fact]
+    public void TwoPlusTwoEqualsFour()
+    {
+        int result = 2 + 2;
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void TwoPlusTwoEqualsFive()
+    {
+        int result = 2 + 2;
+        Assert.NotEqual(5, result);
+    }
+
 
 }
